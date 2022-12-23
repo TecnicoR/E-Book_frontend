90e90218067c8003054a8318f1237bcca301b02d
Rabi <rabinarayanpatra1999@gmail.com>
2022-12-23 20:31:18 +0530

    created product component, added ratings package to show and take ratings

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Product from '../product/Product'
+import './products.css'
+
+function Products() {
+  return (
+    <div className='products'>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+    </div>
+  )
+}
+
+export default Products
